a0914a1f13f1284dc5f3e26ef9e335715af3ac95
nalamichahb <74719889+nalamichahb@users.noreply.github.com>
2020-11-26 10:33:29 +0000

    Update GPO.cc

--- GPO.cc
+++ GPO.cc
@@ -64,7 +64,40 @@ Eigen::VectorXd Residuminimum(Eigen::VectorXd x0, Eigen::VectorXd b, int kmax, E
     cout << "Tolerance non atteinte : " << sqrt(r.dot(r)) << endl ;
   }
 }
+vector<Eigen::Matrix<double, Dynamic, Dynamic>> Arnoldi(Eigen::VectorXd r, Eigen::Matrix<double, Dynamic, Dynamic> A)
+{
+  Eigen::Matrix<double, Dynamic, Dynamic> Vm, Hm ;
+  Eigen::VectorXd v, s, z;
+  vector<Eigen::Matrix<double, Dynamic, Dynamic>> X ;
+  int m = r.size();
+  Eigen::SparseMatrix<double> H(m,m) ;
+  Vm.resize(m,m);
+  v = (1./sqrt(r.dot(r)))*r;
+  Vm.col(0) = v ;
+  for (int j = 0 ; j < m ; ++j )
+  {
+    for (int i = 0 ; i < j ; ++i )
+    {
+      H.insert(i+1,j+1) = Vm.col(j).dot(A*Vm.col(i));
+    }
+    for (int p = 1 ; p < m+1 ; ++p)
+    {
+      s+=H.coeffRef(p,j+1)*Vm.col(p-1);
+    }
+    z = A*Vm.col(j) - s ;
+    H.coeffRef(j+2,j+1) = sqrt(z.dot(z));
+    if (H.coeffRef(j+2,j+1) == 0)
+    {
+      break;
+    }
+    Vm.col(j+1) = (1/H.coeffRef(j+2,j+1))*z;
+  }
+  Hm = MatrixXd(H);
+  X.push_back(Hm);
+  X.push_back(Vm);
 
+  return X;
+}
 
 
 
