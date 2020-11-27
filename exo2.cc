c9cb6f1a21702bc5ffc590911d7574fdaeb5b38a
yarziz <yarziz-houssam12@hotmail.fr>
2020-11-27 23:58:30 +0000

    first

--- exo2.cc
+++ exo2.cc
@@ -3,51 +3,55 @@
 #include <fstream>
 #include <ctime>
 #include <cstdlib>
-#include <vector>
 #include <random>
+#include <Eigen>
 
 using namespace std;
+using namespace Eigen;
 
-vector<vector<double>> transpose(vector<vector<double>> a,int n){
-   vector<vector<double>> b(n,vector<double>(n,0));
-   for(int i=0;i<n;i++){
-     for(int j=0;j<n;j++){
-       b[i][j]=a[j][i];
-     }
-   }
-   return b;
+
+
+
+double somme(Matrix<double,Dynamic,Dynamic> a,int n){
+  double ligne=0.;
+  double max=0.;
+  for(int i=0;i<n;i++){
+    ligne=0.;
+    for(int j=0;j<n;j++){
+      ligne=ligne+abs(a(i,j));
+    }
+    if(ligne>max){
+      max=ligne;
+    }
+  }
+  return max;
 }
 
-double somme(vector<vector<double>> a,int n){
-  double alpha=0;
-   for(int i=0;i<n;i++){
-     for(int j=0;j<n;j++){
-       alpha=alpha+abs(a[i][j]);
-     }
-   }
-   return alpha;
-}
 
 
 
-
-vector<vector<double>> gene_matrice(int n){
-  vector<vector<double>> b(n,vector<double>(n,0));
-  vector<vector<double>> b_bt(n,vector<double>(n,0));
-  vector<vector<double>> a(n,vector<double>(n,0));
+Matrix<double,Dynamic,Dynamic> gene_matrice(int n){
+  Matrix<double,Dynamic,Dynamic> b;
+  Matrix<double,Dynamic,Dynamic> b_bt;
+  Matrix<double,Dynamic,Dynamic> a;
   //double a;
+  b.resize(n,n);
+  b_bt.resize(n,n);
+  a.resize(n,n);
   srand(time(0));
   for(int i=0;i<n;i++){
     for(int j=0;j<n;j++){
-      b[i][j]= ((double) rand())/(double) RAND_MAX;
+      b(i,j)= ((double) rand())/(double) RAND_MAX;
     }
   }
-  b_bt=transpose(b,n)*b;
+  cout<<b<<endl;
+  b_bt=b.transpose()*b;
   b_bt=somme(b_bt,n)*b_bt;
   a=b_bt;
   for(int i=0;i<n;i++){
-    a[i][i]=a[i][i]+1;
+    a(i,i)=a(i,i)+1;
   }
+  return a;
 }
 
 
@@ -56,10 +60,10 @@ vector<vector<double>> gene_matrice(int n){
 
 
 int main(){
-   int n;
-   cout<<"donner n"<<endl;
-   cin>>n;
-   affichage_matrice(gene_matrice(n));
+  int n;
+  cout<<"donner n"<<endl;
+  cin>>n;
+  cout<<gene_matrice(n)<<endl;
  
   return 0;
 }
--- mod_fonction.h
+++
@@ -1,7 +0,0 @@
-#include<vector>
-
-
-void affichage_matrice(std::vector<std::vector<double>> kk,int n);
-
-
-void affichage_vector(std::vector<double> x,int n);
