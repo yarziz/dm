7371eb423a034018b103b43c230048c324c676da
nalamichahb <74719889+nalamichahb@users.noreply.github.com>
2020-12-10 20:35:01 +0000

    Add files via upload

---
+++ slice.cpp
@@ -0,0 +1,10 @@
+void removeRow(Eigen::MatrixXd& matrix, unsigned int rowToRemove)
+{
+    unsigned int numRows = matrix.rows()-1;
+    unsigned int numCols = matrix.cols();
+
+    if( rowToRemove < numRows )
+        matrix.block(rowToRemove,0,numRows-rowToRemove,numCols) = matrix.block(rowToRemove+1,0,numRows-rowToRemove,numCols);
+
+    matrix.conservativeResize(numRows,numCols);
+}
